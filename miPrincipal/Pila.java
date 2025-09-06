c6426e7b89727ef96661cd05ad21565f3924e01d
Jorge Luis <l24171084@culiacan.tecnm.mx>
2025-09-06 21:48:22 +0000

    ci

--- miPrincipal/Pila.java
+++ miPrincipal/Pila.java
@@ -1,4 +1,84 @@
 package miPrincipal;
-public class Pila{
-    
+public class Pila<T>{
+    private T[] pila;
+    private int tope = -1;
+    private T dato;
+    private int max = 0;
+
+    Pila(int maximo) {
+        this.max = maximo;
+        this.pila = (T[]) new Object[max];
+    }
+
+
+    public void Borrarpila() {
+        tope = -1;
+    }
+
+    private boolean Pilallena() {
+        boolean res = false;
+        if (tope == (max - 1)) {
+            res = true;
+        } else {
+            res = false;
+        }
+        return res;
+    }
+
+    private boolean Pilavacia(){
+        boolean res = false;
+        if(tope == -1)
+            res=true;
+        return res;
+    }
+
+    public boolean Insertar(T dato){
+        boolean res= false;
+        if (!Pilallena()){
+            tope++;
+            pila[tope] = dato;
+            res= true;
+        }
+        return res;
+    }
+
+    public T Retirar(){
+            T res;
+            if (!Pilavacia()){
+                res = pila[tope];
+                        tope --;
+            }else{
+                System.out.println("Pila vacia ");
+                res = null;
+            }
+            return res;
+    }
+
+    public void Mostrar(){
+            for(int i = 0;i < tope + 1; i++){
+                System.out.println(pila[i]);
+            }
+    }
+
+    public void invertirPila() {
+        T[] nuevaPila = (T[]) new Object[max];
+        int nuevoTope = 0;
+
+        for (int i = tope; i >= 0; i--) {
+            nuevaPila[nuevoTope++] = pila[i];
+        }
+
+        this.pila = nuevaPila;
+        this.tope = nuevoTope - 1;
+    }
+
+
+    public boolean esPalindromo() {
+        for (int i = 0; i <= tope / 2; i++) {
+            if (!pila[i].equals(pila[tope - i])) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -2,7 +2,50 @@ package miPrincipal;
 
 public class Principal {
     public static void main(String[] args) {
-        
-        
+        Pila<String> pila = new Pila<>(8);
+
+        pila.Insertar("Uno");
+        pila.Insertar("Dos");
+        pila.Insertar("Tres");
+
+        System.out.println("Contenido de la pila:");
+        pila.Mostrar();
+
+        System.out.println("Elemento retirado: " + pila.Retirar());
+
+        System.out.println("Contenido de la pila después de retirar:");
+        pila.Mostrar();
+
+        Cola<Object> cola = new Cola<>(5);
+
+        cola.insertar(10.0);
+        cola.insertar(20.0);
+        cola.insertar(30.0);
+
+        cola.mostrar(); // Cola: 10 20 30
+
+        cola.eliminar();
+        cola.mostrar(); // Cola: 20 30
+
+        ColaCircular<Object> circular = new ColaCircular<>(4);
+
+        circular.insertar("memo");
+        circular.insertar("dos");
+        circular.insertar(2);
+        circular.insertar(4.8);
+
+        circular.mostrar();
+        circular.eliminar();
+        circular.mostrar();
+        circular.insertar("jano");
+        circular.eliminar();
+        circular.mostrar();
+        circular.eliminar();
+        circular.eliminar();
+        circular.insertar("jose");
+        circular.insertar(4);
+        circular.mostrar();
+
+
     }
 }
