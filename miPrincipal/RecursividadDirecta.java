cbefaed62194b4bf844408f1e665a26efce6b094
Jorge Luis <l24171084@culiacan.tecnm.mx>
2025-09-29 17:48:21 +0000

    metodo de potencioa y factorial

--- miPrincipal/RecursividadDirecta.java
+++ miPrincipal/RecursividadDirecta.java
@@ -4,6 +4,8 @@ public class RecursividadDirecta{
     public static void main(String[] args){
         int n = 5;
         System.out.println("la suma de los primeros " + n + " numeros es: " + suma(n));
+        System.out.println("el factrorial de " + n + " es: " + factorial(n));
+        System.out.println("El resultado de " + n + " elevado a la 2 es: "+ potencia(n, 2));
 
     }
 
@@ -17,4 +19,22 @@ public class RecursividadDirecta{
             return n + suma(n-1);
     }
 }
+
+    public static int factorial(int n){
+        if (n==1){
+            return 1;
+        }else{
+            return n* factorial(n-1);
+        }
+    }
+
+    public static int potencia(int n, int e){
+        if (e==0){
+            return 1;
+
+        }else{
+            return n* potencia(n, e-1);
+        }
+
+    }
 }
